c26788ce833cb5ee06523ab135a9a95fb62764d2
NRottmann <Nils.Rottmann@tuhh.de>
2017-05-31 17:05:09 +0200

    Adding the start of the attitude estimator which subscribes to sensor data and calculates the oreintation

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -129,6 +129,8 @@ int px4_simple_app_main(int argc, char *argv[])
         }
 
     // Give actuator input to the HippoC, this will result in a circle
+    act.control[0] = 0.0f;      // roll
+    act.control[1] = 0.0f;      // pitch
 	act.control[2] = 1.0f;		// yaw
     act.control[3] = 1.0f;		// thrust
     orb_publish(ORB_ID(actuator_controls_0), act_pub, &act);
